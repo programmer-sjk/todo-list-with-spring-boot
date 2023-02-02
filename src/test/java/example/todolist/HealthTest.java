57f2fc6334aa8ba4befe1b42b5c34abb8a6faee2
sjk5766 <wjdrnr6662@gmail.com>
2023-02-03 03:12:39 +0900

    feat: spring-boot-starter-actuator 의존성 추가로 health check용 API 추가 & 테스트 코드 추가

---
+++ src/test/java/example/todolist/HealthTest.java
@@ -0,0 +1,29 @@
+package example.todolist;
+
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class HealthTest extends AcceptanceTest {
+	@Test
+	@DisplayName("헬스체크를 통해 Application, DB 구동 상태를 확인할 수 있다.")
+	void health() {
+		// when
+		JsonPath response = RestAssured
+				.given().log().all()
+				.when().get("/health")
+				.then().log().all()
+				.extract()
+				.jsonPath();
+
+		// then
+		String applicationStatus = response.getString("status");
+		String dbStatus = response.getString("components.db.status");
+
+		assertThat(applicationStatus).isEqualTo("UP");
+		assertThat(dbStatus).isEqualTo("UP");
+	}
+}
