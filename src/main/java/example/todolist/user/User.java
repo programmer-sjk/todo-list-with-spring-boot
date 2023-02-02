747f8a72994cca997f4ae4501aa6d8d4f22933f6
sjk5766 <wjdrnr6662@gmail.com>
2023-02-02 16:31:34 +0900

    feat: UserController 추가 및 User 엔티티 마케팅 활용 동의 컬럼 추가

--- src/main/java/example/todolist/user/User.java
+++ src/main/java/example/todolist/user/User.java
@@ -1,6 +1,7 @@
 package example.todolist.user;
 
 import jakarta.persistence.*;
+import org.hibernate.annotations.ColumnDefault;
 
 import java.time.LocalDateTime;
 
@@ -11,14 +12,53 @@ public class User {
     private Long id;
     @Column(length = 20)
     private String name;
-    @Column(length = 50)
+    @Column(length = 50, unique = true, nullable = false)
     private String nickname;
     @Column(unique = true)
     private String email;
+    @Column(nullable = false)
     private String password;
-    @Column(length = 20)
+    @Column(length = 20, unique = true, nullable = false)
     private String phone;
+    @ColumnDefault("'NORMAL'")
     @Enumerated(EnumType.STRING)
     private UserRole role;
     private LocalDateTime deletedAt;
+    private boolean allowMarketing;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public UserRole getRole() {
+        return role;
+    }
+
+    public LocalDateTime getDeletedAt() {
+        return deletedAt;
+    }
+
+    public boolean isAllowMarketing() {
+        return allowMarketing;
+    }
 }
---
+++ src/main/java/example/todolist/user/UserController.java
@@ -0,0 +1,23 @@
+package example.todolist.user;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping()
+    public ResponseEntity<Void> insertUser(@RequestBody User user) {
+        this.userService.insertUser(user);
+        return ResponseEntity.ok().build();
+    }
+}
