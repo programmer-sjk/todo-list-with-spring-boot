b02aabcfb71acf06eba9518da2cc8e20e84ffdc6
sjk5766 <wjdrnr6662@gmail.com>
2023-02-06 15:09:35 +0900

    feat: request dto validation 예외 발생에 대한 Handler 추가

--- src/main/java/example/todolist/exception/GlobalExceptionHandler.java
+++ src/main/java/example/todolist/exception/GlobalExceptionHandler.java
@@ -1,13 +1,29 @@
 package example.todolist.exception;
 
 import example.todolist.common.ResponseMessage;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseMessage<String> handleIllegalArgumentException(IllegalArgumentException e) {
         return ResponseMessage.badRequest(e.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseMessage<String> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        Map<String, String> errors = new HashMap<>();
+        e.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return ResponseMessage.badRequest(errors.toString());
     }
 }
