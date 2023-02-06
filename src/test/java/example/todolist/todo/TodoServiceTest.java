40c91ed874fa0d78af59432c5a2ac29de5f22714
sjk5766 <wjdrnr6662@gmail.com>
2023-02-06 19:54:54 +0900

    refactor: 안 쓰는 import 제거

--- src/test/java/example/todolist/todo/TodoServiceTest.java
+++ src/test/java/example/todolist/todo/TodoServiceTest.java
@@ -22,7 +22,6 @@ import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class TodoServiceTest {
