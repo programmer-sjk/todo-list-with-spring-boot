218a386798c30a40bc6eb8a6ad3937593dd295a7
sjk5766 <wjdrnr6662@gmail.com>
2023-02-16 04:45:47 +0900

    refactor: health check 예외 발생시 early return 하도록 수정

--- src/main/java/example/todolist/filter/HealthFilter.java
+++ src/main/java/example/todolist/filter/HealthFilter.java
@@ -1,17 +1,24 @@
 package example.todolist.filter;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import example.todolist.common.ResponseMessage;
 import example.todolist.health.dto.HealthResponse;
 import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Component;
 import org.springframework.web.util.ContentCachingResponseWrapper;
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 
 @Component
 public class HealthFilter implements Filter {
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
         Filter.super.init(filterConfig);
@@ -26,19 +33,46 @@ public class HealthFilter implements Filter {
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         ContentCachingResponseWrapper responseWrapper =
                 new ContentCachingResponseWrapper((HttpServletResponse) response);
+
         chain.doFilter(request, responseWrapper);
 
-        byte[] responseArray = responseWrapper.getContentAsByteArray();
-        String responseStr = new String(responseArray, responseWrapper.getCharacterEncoding());
+        if (!isValidaHealthResponse(response, responseWrapper)) {
+            responseWrapper.copyBodyToResponse();
+            return ;
+        }
 
-        JsonNode node = new ObjectMapper().readTree(responseStr);
+        JsonNode node = convertJsonNode(responseWrapper);
         String applicationStatus = node.get("status").textValue();
         String dbStatus = node.path("components").path("db").get("status").textValue();
-        HealthResponse healthResponse = new HealthResponse(applicationStatus, dbStatus);
+        ResponseMessage<HealthResponse> message = ResponseMessage.ok(new HealthResponse(applicationStatus, dbStatus));
 
-        String newContent = new ObjectMapper().writeValueAsString(healthResponse);
+        String newContent = objectMapper.writeValueAsString(message);
         response.setContentType("application/json");
         response.setContentLength(newContent.length());
         response.getOutputStream().write(newContent.getBytes());
+    }
+
+    private boolean isValidaHealthResponse(ServletResponse response, ContentCachingResponseWrapper responseWrapper) throws UnsupportedEncodingException, JsonProcessingException {
+        if (((HttpServletResponse) response).getStatus() != HttpServletResponse.SC_OK) {
+            return false;
+        }
+
+        JsonNode statusCodeNode = convertJsonNode(responseWrapper).get("statusCode");
+
+        /**
+         * actuator가 제공하는 health 데이터에는 정상적인 foramt에 statusCode가 없다.
+         * statusCode가 있다는 것은 에러가 발생했다는 의미
+         */
+        if (statusCodeNode != null) {
+            return false;
+        }
+
+        return true;
+    }
+
+    private JsonNode convertJsonNode(ContentCachingResponseWrapper responseWrapper) throws JsonProcessingException, UnsupportedEncodingException {
+        byte[] responseArray = responseWrapper.getContentAsByteArray();
+        String responseStr = new String(responseArray, responseWrapper.getCharacterEncoding());
+        return objectMapper.readTree(responseStr);
     }
 }
