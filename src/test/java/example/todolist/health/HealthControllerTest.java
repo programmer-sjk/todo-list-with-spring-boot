50f884a8849fdddcd897ffcdf6ba1e7fcd3f9b5f
sjk5766 <wjdrnr6662@gmail.com>
2023-02-16 03:45:32 +0900

    test: health check 테스트 수정

--- src/test/java/example/todolist/health/HealthControllerTest.java
+++ src/test/java/example/todolist/health/HealthControllerTest.java
@@ -1,29 +1,27 @@
-package example.todolist;
+package example.todolist.health;
 
+import example.todolist.AcceptanceTest;
+import example.todolist.health.dto.HealthResponse;
 import io.restassured.RestAssured;
-import io.restassured.path.json.JsonPath;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-class HealthTest extends AcceptanceTest {
+class HealthControllerTest extends AcceptanceTest {
 	@Test
 	@DisplayName("헬스체크를 통해 Application, DB 구동 상태를 확인할 수 있다.")
 	void health() {
 		// when
-		JsonPath response = RestAssured
+		HealthResponse response = RestAssured
 				.given().log().all()
 				.when().get("/api/health")
 				.then().log().all()
 				.extract()
-				.jsonPath();
+				.as(HealthResponse.class);
 
 		// then
-		String applicationStatus = response.getString("status");
-		String dbStatus = response.getString("components.db.status");
-
-		assertThat(applicationStatus).isEqualTo("UP");
-		assertThat(dbStatus).isEqualTo("UP");
+		assertThat(response.applicationStatus()).isEqualTo("UP");
+		assertThat(response.dbStatus()).isEqualTo("UP");
 	}
 }
